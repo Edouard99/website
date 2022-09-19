95b3b02c35e412d52e13e2e6c961973b967b75de
Edouard Castets <castetscrassoux@yahoo.fr>
2022-09-19 23:01:36 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { Article, Brand, Cta } from './components'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
+import { Cta, Brand, Navbar} from './components'
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT3</h1>
+    <div className="App">
+        <div className="gradient__bg">
+          <Navbar/>
+          <Header/>
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
     </div>
   )
 }
