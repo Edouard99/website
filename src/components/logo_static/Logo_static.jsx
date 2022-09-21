b892a14000c3b683565bbd989a6e24e1145d8722
Edouard Castets <castetscrassoux@yahoo.fr>
2022-09-21 13:48:41 +0200

    Logo v1

---
+++ src/components/logo_static/Logo_static.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './logo_static.css';
+
+const Logo_static = () => {
+  return (
+        <>
+            <div className='Hexacontainer'>
+
+                
+            </div>
+            <div className='H1'>
+                <div className='hexa_a'>
+                    <span className='hexa_6'></span>
+                    <span className='hexa_3'></span>
+                </div>
+            </div>
+            {/* <div className='H2'>
+                <div className='hexa_b'>
+                    <span className='hexa_1'></span>
+                    <span className='hexa_4'></span>
+                </div>
+            </div>
+            <div className='H3'>
+                <div className='hexa_c'>
+                    <span className='hexa_2'></span>
+                    <span className='hexa_5'></span>
+                </div>
+            </div> */}
+            {/* <div className='H1'>
+                <div className='ec__logo_static-C'>
+                    <span className='ec__logo_static-C_1'></span>
+                    <span className='ec__logo_static-C_2'></span>
+                    <span className='ec__logo_static-C_3'></span>
+                    <span className='ec__logo_static-E_4'></span>
+                </div>
+            </div>
+            <div className='H1'>
+                <div className='ec__logo_static-E'>
+                    <span className='ec__logo_static-E_1'></span>
+                    <span className='ec__logo_static-E_2'></span>
+                    <span className='ec__logo_static-E_3'></span>
+                    <span className='ec__logo_static-E_4'></span>
+                </div>
+
+            </div> */}
+        </>
+  )
+}
+
+export default Logo_static
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 import React, {useState} from 'react';
+import { Logo_static } from '..'
 import './navbar.css';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
+
+const Menu = ()=> (
+  <>
+    <p><a href="#about_me">About Me</a></p>
+    <p><a href="#projects">Projects</a></p>
+    <p><a href="#contact_me">Contact Me</a></p>
+  </>
+)
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -9,25 +18,34 @@ const Navbar = () => {
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
-          <img src={logo} alt="logo"/>
+          {/* <img src={logo} alt="logo"/> */}
+          <Logo_static />
+          
         </div>
-          <div className='gpt3__navbar-links_container'>
-            <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GTP3</a></p>
-            <p><a href="#possibility">OPEN AI</a></p>
-            <p><a href="#features">Case Studies</a></p>
-            <p><a href="#blog">Library</a></p>
+        <div className='gpt3__navbar-links_container'>
+          <Menu />
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>
+        }
+        {toggleMenu && (
+        <div className='gpt3__navbar-menu_container scale-up-center'>
+          <div className='gpt3__navbar-menu_container-links'>
+            <Menu />
           </div>
-          <div className='gpt3__navbar-sign'>
-            <p>Sign in</p>
-            <button type="button">Sign up</button>
-          </div>
-          <div className='gpt3__navbar-menu'>
-            {toggleMenu
-              ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
-              : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>
-            }
-          </div>
+          <div className='gpt3__navbar-menu_container-links-sign'>
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
+            </div>
+        </div>
+        )}
       </div>
     </div>
   )
