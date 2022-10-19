a86c1ef238f270c2aec0a3a88c2066c68088ab30
Edouard Castets <castetscrassoux@yahoo.fr>
2022-10-19 17:32:22 +0200

    up

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -7,20 +7,20 @@ const Menu = ({setLang,lang}) => {
         {lang
           ? 
             <>
-              <p><a href="/#about_me">À propos</a></p>
-              <p><a href="/#projects">Projets</a></p>
-              <p><a href="/#contact_me">Contact</a></p>
-              <p><a href="/#resume">Mon CV</a></p>
+              <p><a href="./#about_me">À propos</a></p>
+              <p><a href="./#projects">Projets</a></p>
+              <p><a href="./#contact_me">Contact</a></p>
+              <p><a href="./#resume">Mon CV</a></p>
               <a title="Switch to English" className='button_lang button_lang_fr' onClick={() =>{setLang(!lang)}}><p></p></a>
               
             </>
           : 
             <>
 
-              <p><a href="/#about_me">About</a></p>
-              <p><a href="/#projects">Projects</a></p>
-              <p><a href="/#contact_me">Contact</a></p>
-              <p><a href="/#resume">Resume</a></p>
+              <p><a href="./#about_me">About</a></p>
+              <p><a href="./#projects">Projects</a></p>
+              <p><a href="./#contact_me">Contact</a></p>
+              <p><a href="./#resume">Resume</a></p>
               <a title="Passer en Francais" className='button_lang button_lang_en' onClick={() =>{setLang(!lang)}}><p></p></a>
             </>
         }
